75c9fb3de6e9db1602fde84349c633db13427af9
anfnamazi <anf.namazi@gmail.com>
2019-10-15 22:51:04 +0330

    add toggle watch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000)
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  toggle() {
+    if (this.state.pause) {
+      this.interval = setInterval(() => this.tick(), 1000)
+      this.setState({
+        pause: false
+      })
+    } else {
+      clearInterval(this.interval)
+      this.setState({
+        pause: true
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div onClick={()=>this.toggle()}>
+        <h2>time is:{this.state.time.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
