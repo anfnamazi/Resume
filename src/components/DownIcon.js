eae6e8340b6d10a93c36f3d13049b6e183936c30
anfnamazi <anf.namazi@gmail.com>
2019-10-25 12:21:20 +0330

    dwn

--- src/components/DownIcon.js
+++ src/components/DownIcon.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 import data from "../data.json";
+import './DownIcon.css'
 
 export default class DownIcon extends Component {
     render() {
         return (
             <div style={this.props.style}>
-                <div style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    transform: 'transLateY( -30px)',
-                    cursor: "pointer",
-                    userSelect: "none"
-                }} >
+                <div className="drop" >
                     <img src={data.icons} style={{ height: "40px", position: "absolute" }} />
                 </div>
             </div>
