c64ab113efc7cc1069d1ad0ed4fad60893305fc1
anfnamazi <anf.namazi@gmail.com>
2024-01-12 23:24:51 +0330

    Updates

--- precache-manifest.891f53fbe8b025e017e7855bfbc4d7da.js
+++ precache-manifest.891f53fbe8b025e017e7855bfbc4d7da.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc56ed936f88c32cbbdc6e82fd3c9ac4",
+    "revision": "348d18fad6a641b1c77d211145b26bca",
     "url": "/Resume/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Resume/static/css/2.47cf6a21.chunk.css"
   },
   {
-    "revision": "6f9740b4a5ba5699ed98",
+    "revision": "ae9f0d3bf232bbe2114c",
     "url": "/Resume/static/css/main.1cc77a7d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Resume/static/js/2.81a91182.chunk.js"
   },
   {
-    "revision": "6f9740b4a5ba5699ed98",
-    "url": "/Resume/static/js/main.86d59527.chunk.js"
+    "revision": "ae9f0d3bf232bbe2114c",
+    "url": "/Resume/static/js/main.5564392b.chunk.js"
   },
   {
     "revision": "ad6fe3719a440415071d",
